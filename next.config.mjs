7b7ae93a3c3177d2035b4469b4cffd6ca6705c5d
Edward Hsing <xingyujie50@gmail.com>
2026-03-23 23:26:28 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    webpackBuildWorker: false,
+  },
 };
 
 export default nextConfig;
